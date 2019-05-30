1a3a32ba793ccad33770f6ded1d50fc17efc4aee
RatonII <mariuss2007@gmail.com>
2019-05-30 23:28:22 +0300

    add video_id to save in mysql comment table

--- public/index.php
+++ public/index.php
@@ -131,12 +131,12 @@ function deleteVideo($request) {
 }
 
 function getComments($request,$response) {
-    $emp = json_decode($request->getBody());
-    $video_id = $emp->video_id;
-   // $video_id = $request->getAttribute('video_id');
+//    $emp = json_decode($request->getBody());
+//    $video_id = $emp->video_id;
+    $video_id = $request->getAttribute('video_id');
     $sql = "SELECT name,message FROM comments WHERE video_id=:video_id";
     try {
-        if (!empty($emp->video_id)) {
+        if (!empty($video_id)) {
             $db = getConnection();
             $stmt = $db->query($sql);
             $stmt->bindParam("video_id", $video_id);
