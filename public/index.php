601df9cad1bfdec0fd16e6676a4366e1ffafb7ce
RatonII <mariuss2007@gmail.com>
2019-05-31 23:14:32 +0300

    add video_id to save in mysql comment table

--- public/index.php
+++ public/index.php
@@ -140,7 +140,7 @@ function getComments($request,$response) {
             $stmt = $db->query($sql);
             //$stmt->bindParam("video_id",$video_id);
             $stmt->execute();
-            $todos = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $todos = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $db = null;
             return $response->withJson($todos, 200);
         }
